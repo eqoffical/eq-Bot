44af03b46992b494714b66d1ebfb681bdfea8609
RedkoDev <redko0111@gmail.com>
2023-06-21 16:16:56 +0300

    tried youtube playing
    but it get error

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 import random
 import config
+import youtube_dl
+import asyncio
 
 token = config.TOKEN
 
@@ -31,5 +33,65 @@ async def on_message(message):
             response = f'Kinda {random.randrange(100)} 🤔'
             await message.channel.send(response)
             return
+
+# Music-bot Part
+
+
+    # Check if the message author is not a bot
+    if message.author.bot:
+        return
+
+    # Check if the message has a valid command
+    if message.content.startswith('play'):
+        command = message.content.split(' ')
+        if len(command) > 1:
+            url = command[1]
+            voice_channel = message.author.voice.channel
+
+            # Connect to the voice channel
+            voice_client = await voice_channel.connect()
+
+            # Download the audio from the YouTube video
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+                'noplaylist': True,
+            }
+
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(url, download=False)
+                url2 = info['formats'][0]['url']
+
+            # Play the audio in the voice channel
+            voice_client.play(discord.FFmpegPCMAudio(url2))
+
+            # Leave the voice channel after the audio finishes playing
+            def after_playing(error):
+                asyncio.run_coroutine_threadsafe(voice_client.disconnect(), client.loop)
+
+            voice_client.source = discord.PCMVolumeTransformer(voice_client.source)
+            voice_client.source.volume = 0.5  # Adjust the volume if needed
+            voice_client.source.after = after_playing
+
+    elif message.content == 'leave':
+        # Check if the message is the 'leave' command
+        voice_client = discord.utils.get(client.voice_clients, guild=message.guild)
+
+        if voice_client and voice_client.is_connected():
+            await voice_client.disconnect()
+
+
+
+
+
+
+
+
+
+
 
 client.run(token)
