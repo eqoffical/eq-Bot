c2247544a1089da43535de1905011b7446317583
RedkoDev <redko0111@gmail.com>
2023-06-22 10:39:28 +0300

    idk

--- main.py
+++ main.py
@@ -56,46 +56,5 @@ async def on_message(message):
     
                 await message.channel.send(response)
                 return
-'''
-# Music-bot Part
-ytdl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-}
-
-@client.event
-async def on_ready():
-    print(f"Bot logged in as {client.user}")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.lower().startswith('play'):
-        url = message.content.split(' ')[1]
-        voice_channel = message.author.voice.channel
-
-        try:
-            voice_client = await voice_channel.connect()
-        except discord.errors.ClientException:
-            voice_client = message.guild.voice_client
-
-        with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
-            info = ytdl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            voice_client.stop()
-            voice_client.play(discord.FFmpegPCMAudio(url2))
-
-    if message.content.lower().startswith('stop'):
-        voice_client = message.guild.voice_client
-        if voice_client and voice_client.is_playing():
-            voice_client.stop()
-            await voice_client.disconnect()
-'''
 
 client.run(token)
